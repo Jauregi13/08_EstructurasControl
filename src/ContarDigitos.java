b0b577d387665566665b23f5dbd7365532dc33d3
Jon Jauregi <Jon Jauregi@DESKTOP-UPFRNRT>
2017-10-19 12:23:11 +0200

    Ultimo ejercicio terminado

--- src/ContarDigitos.java
+++ src/ContarDigitos.java
@@ -10,11 +10,10 @@ public class ContarDigitos {
 		int suma = 0;
 		String digito = Integer.toString(numero);
 		for (int i=0; i < digito.length(); i++){
-			int digito2 = Integer.parseInt(digito.charAt(i));
-			System.out.println(digito2);
+			int digito2 = Integer.parseInt(""+ digito.charAt(i));
 			suma = suma + digito2;
 		}
-		System.out.println(suma);
+		System.out.println("la suma de los digitos del número es " +suma);
 
 	}
 
